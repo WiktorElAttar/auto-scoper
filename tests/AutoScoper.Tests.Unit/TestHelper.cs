2a765e2019448b19f40c542a26d915b3c77dda3f
WiktorElAttar <32872381+WiktorElAttar@users.noreply.github.com>
2025-02-01 20:36:17 +0100

    Add MetadataReferences to compilation

--- tests/AutoScoper.Tests.Unit/TestHelper.cs
+++ tests/AutoScoper.Tests.Unit/TestHelper.cs
@@ -10,10 +10,17 @@ public static class TestHelper
         // Parse the provided string into a C# syntax tree
         var syntaxTree = CSharpSyntaxTree.ParseText(source);
 
+        var references = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(assembly => !assembly.IsDynamic)
+            .Select(assembly => MetadataReference
+                .CreateFromFile(assembly.Location))
+            .Cast<MetadataReference>();
+
         // Create a Roslyn compilation for the syntax tree.
         var compilation = CSharpCompilation.Create(
             assemblyName: "Tests",
-            syntaxTrees: new[] { syntaxTree });
+            syntaxTrees: new[] { syntaxTree },
+            references: references);
 
 
         // Create an instance of our EnumGenerator incremental source generator
@@ -23,7 +30,7 @@ public static class TestHelper
         GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
 
         // Run the source generator!
-        driver = driver.RunGenerators(compilation);
+        driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out _, out _);
 
         // Use verify to snapshot test the source generator output!
         return Verifier
