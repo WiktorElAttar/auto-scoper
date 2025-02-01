daa3832e65ae1b9554831f0eafd6935d893469f0
WiktorElAttar <32872381+WiktorElAttar@users.noreply.github.com>
2025-02-01 10:58:47 +0100

    Implement AutoScopeAttribute

---
+++ src/AutoScoper/AutoScopeAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace AutoScoper;
+
+public static class AutoScopeAttribute
+{
+    public const string FileName = "AutoScopeAttribute.g.cs";
+
+    public const string FullName = "AutoScoper.AutoScopeAttribute";
+
+    public const string Attribute =
+        """
+        namespace AutoScoper
+        {
+            [System.AttributeUsage(System.AttributeTargets.Class)]
+            public class AutoScopeAttribute : System.Attribute
+            {
+            }
+        }
+        """;
+}
+
+// namespace AutoScoper
+// {
+//     [System.AttributeUsage(System.AttributeTargets.Class)]
+//     public class AutoScopeAttribute : System.Attribute
+//     {
+//     }
+// }
---
+++ src/AutoScoper/AutoScopeGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace AutoScoper;
+
+[Generator]
+public class AutoScopeGenerator: IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+            AutoScopeAttribute.FileName,
+            SourceText.From(AutoScopeAttribute.Attribute, Encoding.UTF8)));
+
+        var scopesToGenerate = context.SyntaxProvider
+            .ForAttributeWithMetadataName(
+                AutoScopeAttribute.FullName,
+                predicate: static (node, _) => node is ClassDeclarationSyntax,
+                transform: static (ctx, _) => CreateAutoScopeDetails(ctx))
+            .Where(m => m != null);
+
+        context.RegisterSourceOutput(
+            scopesToGenerate,
+            static (ctx, scopeDetails) => Execute(ctx, scopeDetails));
+    }
+
+    private static ScopeDetails CreateAutoScopeDetails(GeneratorAttributeSyntaxContext ctx)
+        => new (ctx.TargetSymbol.Name);
+
+    private static void Execute(SourceProductionContext ctx, ScopeDetails scopeDetails)
+    {
+        var result = GenerateAutoScopedPartialClass(scopeDetails);
+
+        ctx.AddSource(
+            $"AutoScoper.{scopeDetails.ClassName}.g.cs",
+            SourceText.From(result, Encoding.UTF8));
+    }
+
+    private static string GenerateAutoScopedPartialClass(ScopeDetails scopeDetails)
+    {
+        var sb = new StringBuilder();
+
+        sb.Append($$"""
+public partial class {{scopeDetails.ClassName}}
+{
+    public int GetInt() => 1;
+}
+""");
+
+        return sb.ToString();
+    }
+}
+
+public class ScopeDetails(string className)
+{
+    public string ClassName { get; } = className;
+};
+
